08c828d7680fef1abb2b0aa7edcaacbac7c75120
Jak Fayz <dev@jfayz.com>
2024-05-05 18:26:20 +0300

    Add min size for desktop version

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,11 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.jfayz.testchat.App
+import java.awt.Dimension
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "KotlinProject") {
+        window.minimumSize = Dimension(400, 400)
         App()
     }
 }
